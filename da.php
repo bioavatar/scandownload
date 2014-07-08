c1f5dc8ba485729476d815e5b06738b4e3e7bdc5
bioavatar <outlook.com>
2014-07-08 00:25:26 -0700

    update
    updated all the files

--- da.php
+++ da.php
@@ -1,30 +1,37 @@
 <?php
-$dir = $_POST['link'];
+$dir = $_POST['link'];//from the form
 // Create DOM from URL or file
 
-require_once 'library/simple_html_dom.php';
+require_once 'library/simple_html_dom.php'; //library to be used 
 
 $html = file_get_html($dir);
-// Find all images 
-foreach($html->find('img') as $element){ //every image found is declared as $element
-       echo $element->src . '<br>';
-       $image = $element->src;
+
+foreach($html->find('img') as $element){
+      
+      $image = $element->src;
       $imglink = $dir . $image;
-      $filename = $element->src;
-      $parsed_url = parse_url($filename, PHP_URL_PATH);
-       //$root = realpath($_SERVER["DOCUMENT_ROOT"]);
-       //define ('SITE_ROOT', realpath(dirname(__FILE__)));
-       $str = strpos(parse_url($filename, PHP_URL_HOST),NULL) ;
-       //$path = SITE_ROOT.$parsed_url;
-       if ($str==true) {
-	file_put_contents($filename, file_get_contents($imglink));
-       }
-       else {
-	file_put_contents(getcwd(), file_get_contents($image), FILE_USE_INCLUDE_PATH);
-	//echo parse_url($filename,PHP_URL_PATH);
-       }
+      $host = parse_url($imglink, PHP_URL_HOST); //get the hostname
+      $value = explode(".", $image);
+      $ext = strtolower(array_pop($value));
+      $rand = rand(1000,9999);
+      $filenameOut = __DIR__ . "/images/" . $host . "_" . $rand . "." . $ext;
+      $filenameOut2 = fopen($filenameOut, "wb");
+        $pos = strpos($image, 'data:image');
+   		if ($pos===false){
+		 $parsing = parse_url($image, PHP_URL_HOST);
+ 
+ 			if ($parsing != NULL){
+			     $image2 = fopen($image,"rb");
+			      fwrite($filenameOut2,fread($image2, 1024 * 8),1024 * 8);
+				}
+			else{
+			      $imglink2 = fopen($imglink, "rb");
+			      fwrite($filenameOut2,fread($imglink2, 1024 * 8), 1024 * 8);
+				}
+  		 }
       
-     
-      
-}
+		}
+
+
+//made by F Pedro Jr.
 ?>
--- index.php
+++ index.php
@@ -1,12 +1,57 @@
-//By Fred Pedro JR
+//By Fred Pedro Jr.
 
+
+<!DOCTYPE HTML>
 <html>
-    <body>
-	<form action="da.php" method="post">
-	Link:<INPUT TYPE="Text" Value="" Name="link">
-	<input type="submit" value="Submit">
-            <p>Please follow this format: http://www.domain.com/ </p>
-</form>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Scan Webpage and Download</title>
+<link href="style/style.css" rel="stylesheet" type="text/css">
+<script type="text/javascript" src="js/jquery-1.10.2.min.js"></script>
+<script type="text/javascript" src="js/jquery.form.min.js"></script>
+<script type="text/javascript">
+$(document).ready(function() { 
+	var options = { 
+			target:   '#output',   
+			success:       afterSuccess,  
+			uploadProgress: OnProgress, 
+			resetForm: true       
+		}; 
+		
+	 $('#form').submit(function() { 
+			$(this).ajaxSubmit(options);  			
+			return false; 
+		}); 
+		
 
-    </body>
+function afterSuccess()
+{
+	
+	$('#loading-img').hide(); //hide submit button
+        $("#output").html("Success")
+        $('#submit-btn').show();
+	$('#where').show();
+}
+
+function OnProgress(event, position, total, percentComplete)
+{
+    $('#loading-img').show();
+    $('#submit-btn').hide()
+$("#output").html("")
+
+}
+
+});
+
+</script>
+<body>
+	<form action="da.php" method="post" enctype="multipart/form-data" id="form">
+	Link:<INPUT TYPE="Text" Value="" Name="link">
+	<input type="submit" value="submit" id="submit-btn"><img src="ajax-loader.gif" id="loading-img" style="display:none;" alt="Please Wait"/>
+        <p>Please follow this format: http://www.domain.com/ </p>
+        </form>
+        <div id="output"></div>
+	<div id="where" style="display: none;">To view the images, go to <a href="/images/" >here</a></div>
+
+</body>
 </html>
